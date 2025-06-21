5c3d7edd607be63a9579409dc2948d7346216801
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2025-06-21 11:55:11 +0600

    Routes Added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Layout/Root";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Auth/Login";
+import Register from "../Pages/Auth/Register";
+import AuthLayout from "../Layout/AuthLayout";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [
+      {
+        path: "/login",
+        Component: Login,
+      },
+      {
+        path: "register",
+        Component: Register,
+      },
+    ],
+  },
+]);
