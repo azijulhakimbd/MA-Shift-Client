818747d6fd2aee7f09bdab1d65d478448c404b7b
azijulhakimbd <azijulhakim.sherpur@gmail.com>
2025-06-22 23:03:59 +0600

    Firebase added with private environment

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,14 @@ import { RouterProvider } from "react-router";
 import { router } from "./Routes/Routes.jsx";
 import 'aos/dist/aos.css';
 import Aos from "aos";
+import AuthProvider from "./Context/AuthContext/AuthProvider.jsx";
 Aos.init()
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <div className="urbanist bg-[#EAECED] max-w-7xl mx-auto">
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </div>
   </StrictMode>
 );
